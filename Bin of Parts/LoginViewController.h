4fcf4caeb916a6038027140fe115f53b7f5f1147
Aaron Holland <aaronholla@gmail.com>
2014-02-27 01:15:39 -0500

    Added Login Modal

---
+++ Bin of Parts/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Bin of Parts
+//
+//  Created by Developer on 2/26/14.
+//  Copyright (c) 2014 Bin of Parts, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end

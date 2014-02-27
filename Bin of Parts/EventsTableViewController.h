d32f067ddf449536d27a1f6b769fbe8ca23a96b8
Aaron Holland <aaronholla@gmail.com>
2014-02-27 09:29:49 -0500

    Added View Controllers

---
+++ Bin of Parts/EventsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsTableViewController.h
+//  Bin of Parts
+//
+//  Created by Developer on 2/27/14.
+//  Copyright (c) 2014 Bin of Parts, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsTableViewController : UITableViewController
+
+@end
---
+++ Bin of Parts/KopTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KopTableViewController.h
+//  Bin of Parts
+//
+//  Created by Developer on 2/27/14.
+//  Copyright (c) 2014 Bin of Parts, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KopTableViewController : UITableViewController
+
+@end

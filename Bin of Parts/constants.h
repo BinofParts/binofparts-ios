27f2d6d13f0f448e1abb7b790f6952f9247aca69
Aaron Holland <aaronholla@gmail.com>
2014-03-01 06:50:22 -0500

    Added Constants
    Base url is different if in production or debug.

---
+++ Bin of Parts/constants.h
@@ -0,0 +1,13 @@
+//
+//  constants.h
+//  Bin of Parts
+//
+//  Created by Developer on 3/1/14.
+//  Copyright (c) 2014 Bin of Parts, inc. All rights reserved.
+//
+
+#ifdef DEBUG
+#define kBaseURL @"http://192.168.0.16:3000/api/v1/"
+#else
+#define kBaseURL @"https://binofparts.com/api/v1/"
+#endif

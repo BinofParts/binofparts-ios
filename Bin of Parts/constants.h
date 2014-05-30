bf3785f2c447bea6f58e4476fafd9918cf9d49be
Aaron Holland <aaronholla@gmail.com>
2014-05-30 16:22:00 -0400

    Updated Local IP address for Debug

--- Bin of Parts/constants.h
+++ Bin of Parts/constants.h
@@ -7,8 +7,8 @@
 //
 
 #ifdef DEBUG
-#define kBaseURL @"http://192.168.0.16:3000/api/v1/"
-#define kNoAPIURL @"http://192.168.0.16:3000/"
+#define kBaseURL @"http://192.168.0.10:3000/api/v1/"
+#define kNoAPIURL @"http://192.168.0.10:3000/"
 #else
 #define kBaseURL @"https://binofparts.com/api/v1/"
 #define kNoAPIURL @"https://binofparts.com/"
